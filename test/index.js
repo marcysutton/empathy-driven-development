5cfc5cbc558dbcc07abd3365708794d9ff92b466
Marcy Sutton <marcy.sutton@deque.com>
2018-04-26 20:27:41 +0200

    add integration test for homepage

--- test/index.js
+++ test/index.js
@@ -1,7 +1,44 @@
-// var testPages = ['login-page'];
+const Selenium = require('selenium-webdriver'),
+	AxeBuilder = require('axe-webdriverjs');
+const assert = require('assert');
 
-// testPages.forEach((page) => {
-// 	if (process.argv[3] === page) {
-// 		return page + '.js';
-// 	}
-// });
+const TestUtils = require('./utils.js');
+
+var port = process.env.npm_package_config_port || 3006;
+
+describe('Homepage', function() {
+	this.timeout(10000);
+
+	let driver;
+
+	beforeEach(function(done) {
+		driver = new Selenium.Builder()
+		  .forBrowser('chrome')
+			.build();
+		
+		driver
+			.get(`http://localhost:${port}`)
+		  .then(function() {
+  			done();
+		  })
+	});
+
+	afterEach(function(done) {
+		driver.quit().then(function() {
+			done();
+		});
+	});
+
+	it('should find no violations', function(done) {
+		driver
+			.findElement(Selenium.By.css('.App'))
+		  .then(function() {
+		    new AxeBuilder(driver)
+		      .analyze(function(results) {
+		      	console.log(TestUtils.printViolations(results.violations));
+		        assert.equal(results.violations.length, 0);
+		        done();
+		      });
+		  });
+	});
+});
