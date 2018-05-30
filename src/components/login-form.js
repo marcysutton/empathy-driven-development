692e4a2d96dd160be6f62d790693a3505ed72894
Marcy Sutton <marcy.sutton@deque.com>
2018-05-30 12:24:45 -0700

    fix login form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -4,16 +4,21 @@ class LoginForm extends Component {
     render() {
         let fields = this.props.fields.map((name, index) => {
             return <div className="form-group" key={ index }>
-                <input type="text" placeholder={name} />
+                <label>
+                    { name }
+                    <input type="text" />
+                </label>
             </div>
         });
         return (
             <form className="login-form">
-                <h2>{ this.props.headline }</h2>
-                { fields }
-                <div className="form-group">
-                    <input type="submit" />
-                </div>
+                <fieldset>
+                    <legend><h2>{ this.props.headline }</h2></legend>
+                    { fields }
+                    <div className="form-group">
+                        <input type="submit" />
+                    </div>
+                </fieldset>
             </form>
         )
     }
