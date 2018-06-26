b9ffc031b192fb07625e5da9c07e57e1071711fd
Marcy Sutton <marcy.sutton@deque.com>
2018-06-26 12:40:16 -0400

    add second card flip for Bagley

--- src/pages/card-flip-demo.js
+++ src/pages/card-flip-demo.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react'
 import CardFlip from '../components/card-flip'
 
-const member = {
+const members = [{
   name: 'Rainier McCheddarton',
   headshot: require('../images/rainier-headshot.jpg'),
   subtitle: 'Labradoodle, squeaker, cheese fan',
   bio: 'Doggo ipsum very hand that feed shibe heckin good boys and girls fat boi much ruin diet you are doing me the shock wrinkler length boy, I am bekom fat lotsa pats dat tungg tho shooberino.',
   twitterLink: 'http://twitter.com'
-}
+},
+{
+  name: 'Bagley the Cat',
+  headshot: require('../images/bagley.jpg'),
+  subtitle: 'Siberian floof, supreme loaf',
+  bio: "The door is opening! how exciting oh, it's you, meh drink water out of the faucet. Spend six hours per day washing, but still have a crusty butthole, so lies down or sit on human they not getting up ever, but lick human with sandpaper tongue.",
+  twitterLink: 'http://twitter.com'
+}]
 
 class CardFlipDemo extends Component {
   render() {
     return (
         <div>
             <CardFlip
-              memberName={member.name}
-              image={member.headshot}
-              subtitle={member.subtitle}
-              bio={member.bio}
-              twitterLink={member.twitterLink}
+              member={members[0]}
+              memberName={members[0].name}
+              image={members[0].headshot}
+              subtitle={members[0].subtitle}
+              bio={members[0].bio}
+              twitterLink={members[0].twitterLink}
+            />
+            <CardFlip
+              memberName={members[1].name}
+              image={members[1].headshot}
+              subtitle={members[1].subtitle}
+              bio={members[1].bio}
+              twitterLink={members[1].twitterLink}
             />
         </div>
     )
